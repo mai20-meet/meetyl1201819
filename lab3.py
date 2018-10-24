666d4ada27557c381c8cc1336569eb6f95faddfa
mai20-meet <mai20@meet.mit.edu>
2018-10-24 13:04:46 -0400

    what ever

---
+++ lab3.py
@@ -0,0 +1,18 @@
+import turtle
+# turtle.addshape("nemo.gif")
+# turtle.shape("nemo.gif")
+# turtle.getshapes()
+# turtle.mainloop()
+turtle.speed(0)
+for i in range(1000):
+ 	
+	turtle.forward(200)
+	turtle.right(60)
+	turtle.forward(100)
+	turtle.right(120)
+	turtle.forward(30)
+	turtle.penup()
+	turtle.setposition(0,0)
+	turtle.pendown()
+	turtle.right(1)
+	
